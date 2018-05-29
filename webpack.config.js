5991215336b02e7f5159ea911b68054abc9c0bda
Louis Hoang <bawsing@gmail.com>
2018-05-29 15:59:37 -0700

    Clean up

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,16 @@ module.exports = {
       {
         test: /\.css$/,
         use: [ 'style-loader', 'css-loader' ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
       }
     ]
   },
