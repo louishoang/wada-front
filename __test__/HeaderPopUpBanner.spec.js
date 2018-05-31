83f07590f72fab94ac0e5b43e2d8d5e167768db6
Louis Hoang <bawsing@gmail.com>
2018-05-31 16:33:33 -0700

    Setup basic test for react
    - Install jest, jest-cli, babel-ject, babel-preset-env, enzyme, enzyme-adapter-react-16, react-test-renderer
    - config Jest to include assets dependencies when running test
    - change default script test to use test.

---
+++ __test__/HeaderPopUpBanner.spec.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import HeaderPopUpBanner from '../src/components/presentations/HeaderPopUpBanner';
+import renderer from 'react-test-renderer';
+
+test('something', () => {
+  expect(1+1).toBe(2);
+});
