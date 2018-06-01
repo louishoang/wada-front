8be75f14039352255f2782231852050287438086
Louis Hoang <bawsing@gmail.com>
2018-05-31 22:58:14 -0700

    Write the first test for HeaderPopUpBanner

--- src/components/presentations/HeaderPopUpBanner.spec.js
+++ src/components/presentations/HeaderPopUpBanner.spec.js
@@ -1,7 +1,20 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import HeaderPopUpBanner from './HeaderPopUpBanner';
 
-it('renders without crashing', () => {
-  shallow(<HeaderPopUpBanner />)
+describe('HeaderPopUpBanner', () => {
+  let wrapper;
+  beforeEach(() => { wrapper = mount(<HeaderPopUpBanner />); });
+
+  it('renders without crashing', () => {
+    expect(wrapper.find('div.popup_banner')).toHaveLength(1);
+  });
+
+  it('adds className closed when user click the X button', () => {
+    const spy = jest.spyOn(wrapper.instance(), 'hidePopUp');
+    wrapper.instance().forceUpdate();
+    expect(spy).toHaveBeenCalledTimes(0);
+    wrapper.find('.popup_off_banner').first().simulate('click');
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
 });
