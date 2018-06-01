626d11606bc4290534d0cf146f354aff3e8f5ed8
Louis Hoang <bawsing@gmail.com>
2018-06-01 11:26:47 -0700

    Install eslint globally caused a conflict with yarn test. Testing on CI

--- webpack.config.js
+++ webpack.config.js
@@ -40,7 +40,7 @@ module.exports = {
     })
   ],
   devServer: {
-    contentBase: path.resolve(__dirname, "dist"),
+    contentBase: path.resolve(__dirname, 'dist'),
     historyApiFallback: true
   }
 };
