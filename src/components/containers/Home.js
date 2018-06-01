1c07fe7cefee16250bb826607381c3de24eab05e
Louis Hoang <bawsing@gmail.com>
2018-05-31 18:12:00 -0700

    Fix code style
    Follows code style at https://github.com/airbnb/javascript

--- src/components/containers/Home.js
+++ src/components/containers/Home.js
@@ -1,17 +1,17 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import HeaderPopUpBanner from '../presentations/HeaderPopUpBanner';
 import Header from '../presentations/Header';
 
-class Home extends Component{
-  constructor(){
+class Home extends Component {
+  constructor() {
     super()
   }
 
-  render(){
-    return(
+  render() {
+    return (
       <div>
-        <HeaderPopUpBanner/>
-        <Header/>
+        <HeaderPopUpBanner />
+        <Header />
         Home
       </div>
     )
